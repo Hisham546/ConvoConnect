7f55bc9d4a2049dccada434b459bf2562afe95a3
Hisham <hisham@perfit.today>
2022-10-03 18:19:07 +0530

    Fixed the issues of libraries

---
+++ src/components/dashboard/calls.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+
+import {
+View,
+Image,
+Text,Button,
+StyleSheet,TouchableOpacity,
+TextInput}
+from "react-native";
+import {widthPercentageToDP as wp,heightPercentageToDP as hp} from 'react-native-responsive-screen'
+
+
+ export default function Calls({navigation}){
+
+ return(
+
+
+  <View style={styles.emptyView}>
+   <Text style={styles.text}>Hi</Text>
+
+
+        </View>
+
+ )
+
+
+
+
+
+
+
+
+}
+
+ const  styles =StyleSheet.create({
+
+  emptyView:{
+     flex : 1,
+    width:wp('100'),
+    height:hp('100'),
+     backgroundColor:'white',
+     alignItems:'center',
+     justifyContent:'center'
+
+
+
+  },
+     text:{
+      color:'black'
+
+
+     }
+
+})
--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -7,12 +7,46 @@ Text,
 StyleSheet,TouchableOpacity,
 TextInput}
 from "react-native";
+import {widthPercentageToDP as wp,heightPercentageToDP as hp} from 'react-native-responsive-screen'
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import Calls from "./calls";
+import Chats from "./chats";
+import Status from "./status";
+const Tab = createMaterialTopTabNavigator();
 
  export default function Dashboard ({navigation}) {
 
  return(
  <View style ={styles.container}>
  <Text style={styles.text}>Hi</Text>
+
+    <Tab.Navigator screenOptions={{
+                               tabBarActiveTintColor:'black',
+                               tabBarInactiveTintColor:'gray',
+                               tabBarPressColor:'#fdd42b',
+                               tabBarIndicatorStyle: {
+                               backgroundColor: 'white',
+                               height: 2
+                               },
+                               tabBarScrollEnabled: true,
+                               tabBarLabelStyle: {fontSize: 12,textTransform: 'none'},
+                               tabBarItemStyle: { width: 120, },
+                               tabBarStyle: {
+                               marginLeft:wp('3.50'),
+                               marginTop:hp('1'),
+                               height:hp('5'),
+                               width:wp('93'),
+                               backgroundColor: 'white',
+                               borderRadius:wp('2')
+
+                               },
+                             }} >
+
+
+                <Tab.Screen name="Chats" component={Chats} />
+                <Tab.Screen name="Status" component={Status} />
+                <Tab.Screen name="Calls" component={Calls} />
+          </Tab.Navigator>
 
  </View>
 
