0b16f192ae6348d9c69b60c554351b94b96eae7a
Hisham <hmuhammed546@gmail.com>
2023-08-01 15:52:57 +0530

    structure align

--- src/components/dashboard/chats.js
+++ src/components/dashboard/chats.js
@@ -1,18 +1,19 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 
 import {
-View,
-Image,
-Text,Button,FlatList,Alert,
-StyleSheet,TouchableOpacity,
-TextInput}
-from "react-native";
-import {widthPercentageToDP as wp,heightPercentageToDP as hp} from 'react-native-responsive-screen'
+  View,
+  Image,
+  Text, Button, FlatList, Alert,
+  StyleSheet, TouchableOpacity,
+  TextInput
+}
+  from "react-native";
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen'
 import Contacts from 'react-native-contacts';
 import { PermissionsAndroid } from 'react-native';
 import CardView from 'react-native-cardview'
 
- export default function Chats({navigation}){
+export default function Chats({ navigation }) {
 
   const [contacts, setContacts] = useState([]);
 
@@ -21,157 +22,157 @@ import CardView from 'react-native-cardview'
       id: 1,
       title: 'Sundar',
       time: '1:00 pm',
-      image:'https://legacy.reactjs.org/logo-og.png'
+      image: 'https://legacy.reactjs.org/logo-og.png'
 
     },
     {
       id: 2,
       title: 'Elon',
       time: '4:50 pm',
-            image:'https://legacy.reactjs.org/logo-og.png'
+      image: 'https://legacy.reactjs.org/logo-og.png'
     },
     {
       id: 3,
       title: 'Arnold',
       time: '2:30 pm',
-      image:'https://legacy.reactjs.org/logo-og.png'
+      image: 'https://legacy.reactjs.org/logo-og.png'
 
     },
-      {
-        id: 4,
-        title: 'Ruby',
-        time: '7:00 pm',
-        image:'https://legacy.reactjs.org/logo-og.png'
+    {
+      id: 4,
+      title: 'Ruby',
+      time: '7:00 pm',
+      image: 'https://legacy.reactjs.org/logo-og.png'
 
-      },
-      {
-        id: 5,
-        title: 'Steve Rogers',
-        time: '9:00 pm',
-        image:'https://legacy.reactjs.org/logo-og.png'
+    },
+    {
+      id: 5,
+      title: 'Steve Rogers',
+      time: '9:00 pm',
+      image: 'https://legacy.reactjs.org/logo-og.png'
 
-      },
-      {
-        id: 6,
-        title: 'Robert',
-        time: '9:45 pm',
-        image:'https://legacy.reactjs.org/logo-og.png'
+    },
+    {
+      id: 6,
+      title: 'Robert',
+      time: '9:45 pm',
+      image: 'https://legacy.reactjs.org/logo-og.png'
 
-      },
-     {
+    },
+    {
       id: 7,
       title: 'Sundar',
       time: '1:00 pm',
-      image:'https://legacy.reactjs.org/logo-og.png'
+      image: 'https://legacy.reactjs.org/logo-og.png'
 
     },
     {
       id: 8,
       title: 'Elon',
       time: '4:50 pm',
-       image:'https://legacy.reactjs.org/logo-og.png'
+      image: 'https://legacy.reactjs.org/logo-og.png'
     },
     {
       id: 9,
       title: 'Arnold',
       time: '2:30 pm',
-      image:'https://legacy.reactjs.org/logo-og.png'
+      image: 'https://legacy.reactjs.org/logo-og.png'
 
     },
-      {
-        id: 10,
-        title: 'Ruby',
-        time: '7:00 pm',
-        image:'https://legacy.reactjs.org/logo-og.png'
+    {
+      id: 10,
+      title: 'Ruby',
+      time: '7:00 pm',
+      image: 'https://legacy.reactjs.org/logo-og.png'
 
-      },
-      {
-        id: 11,
-        title: 'Steve Rogers',
-        time: '9:00 pm',
-        image:'https://legacy.reactjs.org/logo-og.png'
+    },
+    {
+      id: 11,
+      title: 'Steve Rogers',
+      time: '9:00 pm',
+      image: 'https://legacy.reactjs.org/logo-og.png'
 
-      },
-      {
-        id: 12,
-        title: 'Robert',
-        time: '9:45 pm',
-        image:'https://legacy.reactjs.org/logo-og.png'
+    },
+    {
+      id: 12,
+      title: 'Robert',
+      time: '9:45 pm',
+      image: 'https://legacy.reactjs.org/logo-og.png'
 
-      },
+    },
   ];
-     const checkPermission = async () => {
-             try {
-               const granted = await PermissionsAndroid.request(
-                 PermissionsAndroid.PERMISSIONS.READ_CONTACTS
-               );
-               console.log('Permission status:', granted);
-               if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-                 getContacts();
-               } else {
-                 Alert.alert(
-                   'Permission Denied!',
-                   'You need to give storage permission to get the contacts'
-                 );
-               }
-             } catch (err) {
-               console.warn(err);
-             }
-           };
+  const checkPermission = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.READ_CONTACTS
+      );
+      console.log('Permission status:', granted);
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        getContacts();
+      } else {
+        Alert.alert(
+          'Permission Denied!',
+          'You need to give storage permission to get the contacts'
+        );
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  };
   useEffect(() => {
-checkPermission()
+    checkPermission()
   }, [])
 
 
- const  getContacts = () => {
+  const getContacts = () => {
 
-  Contacts.getAll().then(contacts => {
-    // contacts returned
-    setContacts(contacts)
+    Contacts.getAll().then(contacts => {
+      // contacts returned
+      setContacts(contacts)
 
-   // console.log(contacts,'.....contactz')
- for (let i = 0; i < contacts.length; i++) {
-      const rawContactId = contacts[i].rawContactId;
-     // console.log('Raw Contact ID:', rawContactId);
-    setUniqueKey(rawContactId)
-      // Do something with the rawContactId
-    }
-  })
+      // console.log(contacts,'.....contactz')
+      for (let i = 0; i < contacts.length; i++) {
+        const rawContactId = contacts[i].rawContactId;
+        // console.log('Raw Contact ID:', rawContactId);
+        setUniqueKey(rawContactId)
+        // Do something with the rawContactId
+      }
+    })
   }
-const image = 'https://legacy.reactjs.org/logo-og.png';
- return(
+  const image = 'https://legacy.reactjs.org/logo-og.png';
+  return (
 
-          <View style={styles.emptyView}>
-               <FlatList
-                  data={DATA}
-                  renderItem={({item}) =>
-             <TouchableOpacity activeOpacity={0.9}  onPress={() => navigation.navigate('Interface',{data:item,image})}>
-                <CardView
-                  cardElevation={2}
-                  cardMaxElevation={2} style={styles.chatCard}>
-                     <View style={styles.userChatBox}>
-                   <Image resizeMode="cover"  style={styles.tinyLogo} source={{uri: image}}  />
-                   <Text style={styles.name}>{item.title}</Text>
-                  {/* <Text style={styles.name}>{item.displayName}</Text>*/}
+    <View style={styles.emptyView}>
+      <FlatList
+        data={DATA}
+        renderItem={({ item }) =>
+          <TouchableOpacity activeOpacity={0.9} onPress={() => navigation.navigate('Interface', { data: item, image })}>
+            <CardView
+              cardElevation={2}
+              cardMaxElevation={2} style={styles.chatCard}>
+              <View style={styles.userChatBox}>
+                <Image resizeMode="cover" style={styles.tinyLogo} source={{ uri: image }} />
+                <Text style={styles.name}>{item.title}</Text>
+                {/* <Text style={styles.name}>{item.displayName}</Text>*/}
 
-                    </View>
-                      <View style={styles.userChatBox2}>
-                                         <Text style={{fontSize:hp('1.50'),color:'black',marginRight:wp('3')}}>{item.time}</Text>
-                                            </View>
-                 </CardView>
-                   </TouchableOpacity>}
+              </View>
+              <View style={styles.userChatBox2}>
+                <Text style={{ fontSize: hp('1.50'), color: 'black', marginRight: wp('3') }}>{item.time}</Text>
+              </View>
+            </CardView>
+          </TouchableOpacity>}
 
-                   keyExtractor={item => item.id}
-                />
-
-
+        keyExtractor={item => item.id}
+      />
 
 
 
 
-        </View>
 
- )
+
+    </View>
+
+  )
 
 
 
@@ -182,50 +183,50 @@ const image = 'https://legacy.reactjs.org/logo-og.png';
 
 }
 
- const  styles =StyleSheet.create({
+const styles = StyleSheet.create({
 
-  emptyView:{
-      flex : 1,
-      width:wp('100'),
-      height:hp('100'),
-      backgroundColor:'white',
+  emptyView: {
+    flex: 1,
+    width: wp('100'),
+    height: hp('100'),
+    backgroundColor: 'white',
   },
-   text:{
-       color:'black'
-
-   },
-   chatCard:{
-          width:wp('100'),
-          height:hp('10'),
-          backgroundColor:'white',
-          flexDirection:'row'
-  },
-  tinyLogo:{
-      height: 40,
-      width: 40,
-      borderRadius:20,
-      marginLeft:wp('5'),
+  text: {
+    color: 'black'
 
   },
-  userChatBox:{
-      width:wp('50'),
-      height:hp('8'),
-      flexDirection:'row',
-      alignItems:'center'
+  chatCard: {
+    width: wp('100'),
+    height: hp('10'),
+    backgroundColor: 'white',
+    flexDirection: 'row'
+  },
+  tinyLogo: {
+    height: 40,
+    width: 40,
+    borderRadius: 20,
+    marginLeft: wp('5'),
 
   },
-  userChatBox2:{
-    width:wp('50'),
-    height:hp('8'),
-   flexDirection:'row',
-   alignItems:'center',
-   justifyContent:'flex-end',
+  userChatBox: {
+    width: wp('50'),
+    height: hp('8'),
+    flexDirection: 'row',
+    alignItems: 'center'
+
   },
-  name:{
-      marginLeft:wp('5'),
-      marginBottom:hp('3'),
-      color:'black',
-      fontFamily:'Manrope-Regular'
+  userChatBox2: {
+    width: wp('50'),
+    height: hp('8'),
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+  },
+  name: {
+    marginLeft: wp('5'),
+    marginBottom: hp('3'),
+    color: 'black',
+    fontFamily: 'Manrope-Regular'
 
   }
 
