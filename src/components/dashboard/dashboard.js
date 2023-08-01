25675cb8a06ec98b3f81b2f15b6fa26a430e079a
Hisham <hmuhammed546@gmail.com>
2023-08-01 15:54:48 +0530

    structure align

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -1,12 +1,12 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 import {
-View,
-Text,Alert,
-StyleSheet,TouchableOpacity,
+  View,
+  Text, Alert,
+  StyleSheet, TouchableOpacity,
 }
-from "react-native";
-import {widthPercentageToDP as wp,heightPercentageToDP as hp} from 'react-native-responsive-screen'
+  from "react-native";
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen'
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import MaterialIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 import Calls from "./calls";
@@ -17,150 +17,150 @@ import { useSelector, useDispatch } from "react-redux";
 import Camera from '../../camera/camera';
 import { openModalPopup } from '../../state/counterReducer';
 import { PermissionsAndroid } from 'react-native';
- import { Menu, MenuItem,  } from 'react-native-material-menu';
+import { Menu, MenuItem, } from 'react-native-material-menu';
 
- export default function Dashboard ({navigation}) {
+export default function Dashboard({ navigation }) {
 
   const [visible, setVisible] = useState(false);
 
   const hideMenu = () => setVisible(false);
-   function goTOSettings() {
-   navigation.navigate('Settings')
-   setVisible(false);
+  function goTOSettings() {
+    navigation.navigate('Settings')
+    setVisible(false);
 
-   }
+  }
 
   const showMenu = () => setVisible(true);
-    const openModal = useSelector((state) => state.counter.openModal);
+  const openModal = useSelector((state) => state.counter.openModal);
 
-    const dispatch= useDispatch()
+  const dispatch = useDispatch()
 
- //--To check the user granted the necessary permissions or not
-   const checkCameraPermission = async () => {
-               try {
-                 const granted = await PermissionsAndroid.request(
-                   PermissionsAndroid.PERMISSIONS.CAMERA
-                 );
-                 console.log('Permission status:', granted);
-                 if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-                   dispatch(openModalPopup(true));
-                 } else {
-                   Alert.alert(
-                     'Permission Denied!',
-                     'You need to give storage permission to get the contacts'
-                   );
-                 }
-               } catch (err) {
-                 console.warn(err);
-               }
-             };
-
-
- return(
+  //--To check the user granted the necessary permissions or not
+  const checkCameraPermission = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.CAMERA
+      );
+      console.log('Permission status:', granted);
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        dispatch(openModalPopup(true));
+      } else {
+        Alert.alert(
+          'Permission Denied!',
+          'You need to give storage permission to get the contacts'
+        );
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  };
 
 
- <View style ={styles.container}>
- <View style ={styles.topViewContainer}>
- <Text style={{color:'white',marginLeft:wp('4'),marginTop:hp('3'),fontSize:hp('2.20')}}>WhatsApp</Text>
-    <TouchableOpacity activeOpacity={1} onPress={() => checkCameraPermission() }>
-                 <MaterialIcon name={'camera-enhance'} size={hp('2.65%')} color={'white'}  style={styles.cameraIcon} />
+  return (
+
+
+    <View style={styles.container}>
+      <View style={styles.topViewContainer}>
+        <Text style={{ color: 'white', marginLeft: wp('4'), marginTop: hp('3'), fontSize: hp('2.20') }}>WhatsApp</Text>
+        <TouchableOpacity activeOpacity={1} onPress={() => checkCameraPermission()}>
+          <MaterialIcon name={'camera-enhance'} size={hp('2.65%')} color={'white'} style={styles.cameraIcon} />
         </TouchableOpacity>
-      {openModal ? <Camera /> : null}
-        <MaterialIcon name={'magnify'} size={hp('2.65%')} color={'white'}  style={styles.searchIcon} />
+        {openModal ? <Camera /> : null}
+        <MaterialIcon name={'magnify'} size={hp('2.65%')} color={'white'} style={styles.searchIcon} />
 
-      {/*  <TouchableOpacity onPress={()=>navigation.navigate('Settings')}>
+        {/*  <TouchableOpacity onPress={()=>navigation.navigate('Settings')}>
              <MaterialIcon name={'dots-vertical'} size={hp('2.65%')} color={'white'}  style={styles.threeDotIcon} />
              </TouchableOpacity>*/}
-                    <Menu
-                          visible={visible}
-                          style ={styles.menuStyle}
-                          anchor={
-                                  <TouchableOpacity activeOpacity={1} onPress={showMenu}>
-                                    <MaterialIcon name={'dots-vertical'} size={hp('2.65%')} color={'white'}  style={styles.threeDotIcon} />
-                                  </TouchableOpacity>
-                                 }
-                    onRequestClose={hideMenu}>
-                           <MenuItem textStyle ={styles.menuTextStyle} onPress={() => goTOSettings()}>Settings</MenuItem>
-                              <MenuItem  textStyle ={styles.menuTextStyle} onPress={hideMenu}>About</MenuItem>
+        <Menu
+          visible={visible}
+          style={styles.menuStyle}
+          anchor={
+            <TouchableOpacity activeOpacity={1} onPress={showMenu}>
+              <MaterialIcon name={'dots-vertical'} size={hp('2.65%')} color={'white'} style={styles.threeDotIcon} />
+            </TouchableOpacity>
+          }
+          onRequestClose={hideMenu}>
+          <MenuItem textStyle={styles.menuTextStyle} onPress={() => goTOSettings()}>Settings</MenuItem>
+          <MenuItem textStyle={styles.menuTextStyle} onPress={hideMenu}>About</MenuItem>
 
 
-                             </Menu>
+        </Menu>
 
-</View>
-    <Tab.Navigator
-          screenOptions={{
-                         tabBarActiveTintColor:'gray',
-                         tabBarInactiveTintColor:'gray',
-                         tabBarPressColor:'green',
-                         tabBarIndicatorStyle: {
-                         backgroundColor: 'green',
-                         height: 2
-                         },
-                         tabBarScrollEnabled: true,
-                         tabBarLabelStyle: {fontSize:hp('1.40'),textTransform: 'none'},
-                         tabBarItemStyle: { width:wp('33') },
-                         tabBarStyle: {
-                           height:hp('5'),
-                           width:wp('100'),
-                           backgroundColor: 'white',
+      </View>
+      <Tab.Navigator
+        screenOptions={{
+          tabBarActiveTintColor: 'gray',
+          tabBarInactiveTintColor: 'gray',
+          tabBarPressColor: 'green',
+          tabBarIndicatorStyle: {
+            backgroundColor: 'green',
+            height: 2
+          },
+          tabBarScrollEnabled: true,
+          tabBarLabelStyle: { fontSize: hp('1.40'), textTransform: 'none' },
+          tabBarItemStyle: { width: wp('33') },
+          tabBarStyle: {
+            height: hp('5'),
+            width: wp('100'),
+            backgroundColor: 'white',
 
-                         },
-                      }} >
-
-
-                <Tab.Screen name="CHATS" component={Chats} />
-                <Tab.Screen name="STATUS" component={Status} />
-                <Tab.Screen name="CALLS" component={Calls} />
-          </Tab.Navigator>
-
- </View>
+          },
+        }} >
 
 
- );
- }
+        <Tab.Screen name="CHATS" component={Chats} />
+        <Tab.Screen name="STATUS" component={Status} />
+        <Tab.Screen name="CALLS" component={Calls} />
+      </Tab.Navigator>
 
-  const  styles =StyleSheet.create({
-
-
-  container:{
-      flex:1,
-        backgroundColor:'#128C7E',
-   },
-   topViewContainer:{
-   width:wp('100'),
-   height:hp('10'),
-
-   flexDirection:'row',
-   justifyContent:'space-between'
-   },
-   searchIcon:{
-    marginTop:hp('3'),
-    marginLeft:wp('4.50')
-
-   },
-   cameraIcon:{
-      marginTop:hp('3'),
-          marginLeft:wp('35')
-
-   },
-   threeDotIcon:{
-      marginTop:hp('3'),
-      marginRight:wp('4')
-   },
- menuStyle:{
-       backgroundColor :'white',
-       width:wp('40'),
-       marginLeft:wp('2.50')
-
- },
- menuTextStyle:{
- color:'black',
- fontSize:hp('1.50'),
- fontFamily:'Manrope-Regular'
-
- },
+    </View>
 
 
+  );
+}
+
+const styles = StyleSheet.create({
 
 
-  });
+  container: {
+    flex: 1,
+    backgroundColor: '#128C7E',
+  },
+  topViewContainer: {
+    width: wp('100'),
+    height: hp('10'),
+
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  searchIcon: {
+    marginTop: hp('3'),
+    marginLeft: wp('4.50')
+
+  },
+  cameraIcon: {
+    marginTop: hp('3'),
+    marginLeft: wp('35')
+
+  },
+  threeDotIcon: {
+    marginTop: hp('3'),
+    marginRight: wp('4')
+  },
+  menuStyle: {
+    backgroundColor: 'white',
+    width: wp('40'),
+    marginLeft: wp('2.50')
+
+  },
+  menuTextStyle: {
+    color: 'black',
+    fontSize: hp('1.50'),
+    fontFamily: 'Manrope-Regular'
+
+  },
+
+
+
+
+});
