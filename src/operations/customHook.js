6ba00a7b41f46331e162c6c69c9574883cdaf1a7
Muhammed Hisham <hmuhammed546@gmail.com>
2024-05-15 15:47:13 +0530

    removed all files

--- src/operations/customHook.js
+++
@@ -1,9 +0,0 @@
-// useRealmCustomHook.js
-import { useRealm } from '@realm/react';
-
-const useRealmCustomHook = () => {
-  const realm = useRealm();
-  return realm;
-};
-
-export default useRealmCustomHook;
